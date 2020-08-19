9101c8bc55d7497e308e5e548341583ac97c2f8a
DaviRain-Su <davirain.yin@gmail.com>
2020-08-20 00:49:39 +0800

    add some comment of slice about cpp and python

--- palindromic_substrings/src_py/main.py
+++ palindromic_substrings/src_py/main.py
@@ -1,6 +1,41 @@
 # 代码超时
 #
 #
+# python string slice
+# 
+# 切片操作（slice）可以从一个字符串中获取子字符串（字符串的一部分）。我们使用一对方括号、起始偏移量start、终止偏移量end 以及可选的步长step 来定义一个分片。
+#
+# 格式： [start:end:step]
+#
+# • [:] 提取从开头（默认位置0）到结尾（默认位置-1）的整个字符串
+# • [start:] 从start 提取到结尾
+# • [:end] 从开头提取到end - 1
+# • [start:end] 从start 提取到end - 1
+# • [start:end:step] 从start 提取到end - 1，每step 个字符提取一个
+# • 左侧第一个字符的位置/偏移量为0，右侧最后一个字符的位置/偏移量为-1
+#
+#
+#
+# 几个特别的examples 如下：
+#
+# 提取最后N个字符：
+#
+# >>> letter = 'abcdefghijklmnopqrstuvwxyz'
+# >>> letter[-3:]
+# 'xyz'
+#
+# 从开头到结尾，step为N：
+#
+# >>> letter[::5]
+# 'afkpuz'
+#
+# 将字符串倒转(reverse)， 通过设置步长为负数：
+#
+# >>> letter[::-1]
+# 'zyxwvutsrqponmlkjihgfedcba'
+
+
+
 def is_palindromic(s: str) -> bool:
     length: int = len(s)
     for i in range(length // 2):
